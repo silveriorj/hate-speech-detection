2bdbbfe885928add06fff2d7300227964887fb71
Raul Silverio <raul.silverio@sap.com>
2025-06-20 19:03:43 -0300

    feat: improved notebook and finished related article

--- dataset.py
+++ dataset.py
@@ -7,26 +7,29 @@ class HateSpeechDataset(Dataset):
         self.labels = labels
         self.tokenizer = tokenizer
         self.max_length = max_length
+        self.encoded_texts = []
+        self.__encode__()
+        
+    def __encode__(self):
+        for text in self.texts:
+            encoding = self.tokenizer.encode_plus(
+                text,
+                add_special_tokens=True,
+                max_length=self.max_length,
+                padding='max_length',
+                truncation=True,
+                return_attention_mask=True,
+                return_token_type_ids=False,
+                return_tensors='pt'
+            )
+            self.encoded_texts.append(encoding)
 
     def __len__(self):
         return len(self.texts)
 
     def __getitem__(self, idx):
-        text = self.texts[idx]
-
-        encoding = self.tokenizer.encode_plus(
-            text,
-            add_special_tokens=True,
-            max_length=self.max_length,
-            padding='max_length',
-            truncation=True,
-            return_attention_mask=True,
-            return_token_type_ids=False,
-            return_tensors='pt'
-        )
-
         return {
-            'input_ids': encoding['input_ids'].flatten(),
-            'attention_mask': encoding['attention_mask'].flatten(),
+            'input_ids': self.encoded_texts[idx]['input_ids'].flatten(),
+            'attention_mask': self.encoded_texts[idx]['attention_mask'].flatten(),
             'labels': torch.FloatTensor(self.labels[idx]),
         }
